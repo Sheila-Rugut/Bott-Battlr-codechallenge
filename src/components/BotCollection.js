1f209f916dea383150cfd42416338fcecf6850f0
Sheila <rugut.cheruto@student.moringaschool.com>
2022-10-02 16:49:48 +0300

    display bots

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-function BotCollection() {
+
+function BotCollection({bots}) {
   // Your code here
+
   return (
     <div className="ui four column grid">
       <div className="row">
         {/*...and here..*/}
         Collection of all bots
+        {bots.map(bot=> (<span key={bot.id}><img src={bot.avatar_url} alt="Bot"/></span>))}
       </div>
     </div>
   );
